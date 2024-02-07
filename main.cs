57b54a8cd727a790e27345eb41dba5d0172ebd35
hed2000 <hed2000@hw.ac.uk>
2024-02-07 15:12:38 +0000

    Added database connection

--- main.cs
+++ main.cs
@@ -1,8 +1,12 @@
 using Godot;
 using System;
 
+using MongoDB.Driver;
+using MongoDB.Bson;
+
 public partial class main : Node
 {
+		
 	private int Money;
 	
 	// Called when the node enters the scene tree for the first time.
@@ -11,6 +15,24 @@ public partial class main : Node
 		Money = 150;
 		var label = GetNode<Label>("MoneyLabel");
 		label.Text = Money.ToString();
+		
+		const string connectionUri = "mongodb+srv://application:application4password@cluster0.64h8yuk.mongodb.net/?retryWrites=true&w=majority";
+		var settings = MongoClientSettings.FromConnectionString(connectionUri);
+
+		// Set the ServerApi field of the settings object to set the version of the Stable API on the client
+		settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+
+		// Create a new client and connect to the server
+		var client = new MongoClient(settings);
+
+		// Send a ping to confirm a successful connection
+		try {
+		  var result = client.GetDatabase("admin").RunCommand<BsonDocument>(new BsonDocument("ping", 1));
+		  GD.Print("Pinged your deployment. You successfully connected to MongoDB!");
+		} catch (Exception ex) {
+		 GD.Print(ex);
+		}
+		
 	}
 	
 	private void _on_add_button_pressed()
