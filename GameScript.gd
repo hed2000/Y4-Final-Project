fd7521e6ce52fdee1f2d7be6d86f4f542c5285e9
hed2000 <hed2000@hw.ac.uk>
2024-02-10 10:37:09 +0000

    Converted main game script to GDscript

--- GameScript.gd
+++ GameScript.gd
@@ -1,13 +1,24 @@
 extends Node
 
-
+var Money
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Money = 150;
+	%MoneyLabel.text = "%d" % Money
 	
 func _on_button_2_pressed():
 	var catsprite = %catsprite
 	catsprite.play("hat")
+	
+func _on_add_button_pressed():
+	Money += 100
+	%MoneyLabel.text = "%d" % Money
+
+func _on_button_1_pressed():
+	%shop.show()
+	
+func _on_shop_exit_button_pressed():
+	%shop.hide()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- main.cs
+++ main.cs
@@ -3,11 +3,13 @@ using System;
 
 using MongoDB.Driver;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
 
 public partial class main : Node
 {
 		
 	//private int Money;
+	private static IMongoCollection<User> _usersCollection;
 	
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -15,6 +17,10 @@ public partial class main : Node
 		//Money = 150;
 		//var label = GetNode<Label>("MoneyLabel");
 		//label.Text = Money.ToString();
+		
+		// Allows automapping of the camelCase database fields to models 
+		var camelCaseConvention = new ConventionPack { new CamelCaseElementNameConvention() };
+		ConventionRegistry.Register("CamelCase", camelCaseConvention, type => true);
 		
 		const string connectionUri = "mongodb+srv://application:application4password@cluster0.64h8yuk.mongodb.net/?retryWrites=true&w=majority";
 		var settings = MongoClientSettings.FromConnectionString(connectionUri);
@@ -24,6 +30,8 @@ public partial class main : Node
 
 		// Create a new client and connect to the server
 		var client = new MongoClient(settings);
+		var applicationDatabase = client.GetDatabase("application");
+		_usersCollection = applicationDatabase.GetCollection<User>("user");
 
 		// Send a ping to confirm a successful connection
 		try {
@@ -33,8 +41,27 @@ public partial class main : Node
 		 GD.Print(ex);
 		}
 		
+		// start-find-builders
+		// Creates a filter for all documents that have a "name" value of "Bagels N Buns"
+		var filter = Builders<User>.Filter
+			.Eq(r => r.Name, "hed2000");
+
+		// Retrieves the first document that matches the filter
+		var user = _usersCollection.Find(filter).FirstOrDefault();
+		// end-find-builders
+
+		Console.WriteLine(user.ToBsonDocument());
+		
 	}
-	
-	
-	
+
+}
+
+public class User
+{
+	public string Id { get; set; }
+	public string Name { get; set; }
+	public Int32 Exp { get; set; }
+	public Int32 Money { get; set; }
+	public string PetType { get; set; }
+	public string Password { get; set; }
 }
